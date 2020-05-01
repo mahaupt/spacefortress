c06ec28d010357cd76d89cf27d3af1a7944512b9
Marcel Haupt <subcortexx@gmail.com>
2020-05-01 15:59:30 +0200

    real time computation with timer

--- main.cpp
+++ main.cpp
@@ -6,29 +6,34 @@
 #include "modules/generator.hpp"
 
 int main()
-{
-    double refresh_rate = 1.0/1.0;
-    
+{    
     Ship s = Ship("Omega", 100, 100);
     s.addFuel(100);
     s.addModule(new Generator("Generator MKI", 10, 1, 1));
     
     std::string cmd = "";
+    double simTime = 0.1; 
     
-    do {
+    while(true) {
+        auto start_time = std::chrono::steady_clock::now();
+        
         std::cout << "\033c";
-        s.simulate(refresh_rate);
+        s.simulate(simTime);
         s.info();
+        std::cout << "Simulating " << simTime << " seconds" << std::endl;
         
-        /*char c;
-        std::cin.read(&c, sizeof(c));
-        cmd = cmd + c;
         
-        std::cout << std::endl;
-        std::cout << "> " << cmd;*/
         
-        std::this_thread::sleep_for(std::chrono::milliseconds((int)(refresh_rate*1000)));
-    } while(true);
+        std::cin >> cmd;
+        if (cmd == "q")
+        {
+            break;
+        }
+        
+        auto end_time = std::chrono::steady_clock::now();
+        simTime = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count()/1000.0;
+        
+    }
     
     
     return 0;
--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-g++ main.cpp ship.cpp module.cpp modules/generator.cpp -o spacefortress
+g++ main.cpp ship.cpp module.cpp modules/generator.cpp -o spacefortress -Wno-c++11-extensions
