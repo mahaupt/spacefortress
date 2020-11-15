8cdbc24ced8b9e92b3793b356606b7be0924ac48
cbacon93 <subcortexx@gmail.com>
2020-11-15 13:16:24 +0100

    added src directory, edited make.sh file

--- make.sh
+++ make.sh
@@ -1,1 +1,12 @@
-g++ main.cpp ship.cpp module.cpp modules/generator.cpp -o spacefortress -Wno-c++11-extensions
+CXX="g++"
+ROOT="src"
+FLAGS="-Wno-c++11-extensions"
+OUTPUT="-o spacefortress"
+SRC="\
+    $ROOT/main.cpp \
+    $ROOT/ship.cpp \
+    $ROOT/module.cpp \
+    $ROOT/modules/generator.cpp \
+"
+
+$CXX $FLAGS $SRC $OUTPUT
